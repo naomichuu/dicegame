d2405607beacd4e956cb27063f900b2008abc4fb
Naomi Chu <60352633+naomichuu@users.noreply.github.com>
2020-05-26 18:39:12 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function gameRound(){
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
+
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+
+
+  if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML= "🚩Player1 Win!";
+  }
+  else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML= "Player2 Win! 🚩";
+  } else {
+    document.querySelector("h1").innerHTML= "Draw";
+  }
+}
